523a9646cfcc95fdae650ccb460f337c7aff481b
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-12-26 00:22:03 +0530

    navbar commplete

--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -1,19 +1,29 @@
 const Navbar = () => {
-    return(
-        <nav>
-            <div>
-                Logo 
-            </div>
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-black text-white shadow-lg z-50">
+      <div className="max-w-screen-xl mx-auto px-8 flex justify-between items-center h-12">
+        <div className="text-xl font-bold">Logo</div>
 
-            <div>
-                <a href="">About Me</a>
-                <a href="">Experience</a>
-                <a href="">Skills</a>
-                <a href="">Projects</a>
-                <a href="">Contact Me</a>
-            </div>
-        </nav>
-    )
-}
+        <div className="flex space-x-3">
+          <a href="#aboutme" className="cursor-pointer hover:text-gray-400">
+            About Me
+          </a>
+          <a href="#experience" className="cursor-pointer hover:text-gray-400">
+            Experience
+          </a>
+          <a href="#skills" className="cursor-pointer hover:text-gray-400">
+            Skills
+          </a>
+          <a href="#projects" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#contacts" className="cursor-pointer hover:text-gray-400">
+            Contact Me
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
