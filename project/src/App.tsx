a8a2fada38e0d84a700e603daaac778409d3ffac
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-12-26 23:08:16 +0530

    Icons added

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,13 @@
 import Aboutme from "./components/Aboutme";
 import Navbar from "./components/Navbar"
+import Skills from "./components/Skills";
 
 function App(){
   return(
     <div>
       <Navbar />
       <Aboutme />
+      <Skills />
     </div>
   )
 }
--- project/src/components/Skills.tsx
+++ project/src/components/Skills.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'devicon/devicon.min.css';
 
 // defining the structure 
 interface Skill {
