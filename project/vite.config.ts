a12c2b3142d01e3d5f126b535540f6d985be8799
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-12-28 02:12:29 +0530

    gh pages

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,16 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+  base: '/portfolio/', // Replace <repo-name> with your GitHub repo name
+});
