cb3b5b0b95006731889af0a8383bbfeadf73b460
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-12-28 00:13:20 +0530

    navbar transparent

--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 
 const Navbar = () => {
-
   const [isScrollingDown, setIsScrollingDown] = useState(false);
   const [lastScrollPosition, setLastScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
-      setIsScrollingDown(currentScrollPos > lastScrollPosition && currentScrollPos > 50);
+      setIsScrollingDown(
+        currentScrollPos > lastScrollPosition && currentScrollPos > 50
+      );
       setLastScrollPosition(currentScrollPos);
     };
 
@@ -17,7 +18,15 @@ const Navbar = () => {
   }, [lastScrollPosition]);
 
   return (
-    <nav className="fixed top-0 left-0 w-full  shadow-lg z-50">
+    <nav
+      className={`fixed top-0 left-0 w-full shadow-lg z-50 transition-transform duration-300 ${
+        isScrollingDown ? "-translate-y-full" : "translate-y-0"
+      } ${
+        lastScrollPosition > 50
+          ? "bg-white/70 backdrop-blur-lg"
+          : "bg-transparent"
+      }`}
+    >
       <div className="max-w-screen-xl mx-auto px-8 flex justify-between items-center h-20">
         <div className="text-2xl font-bold">Logo</div>
 
